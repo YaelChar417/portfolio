60ba10b2f52872802268de5d43be1c699700c34a
Yael Charles <yaelcharles417@gmail.com>
2025-07-30 12:13:01 -0600

    Feature: funcion en controller creada para mejor estructura MVC

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,2 +1,6 @@
 import { Request, Response } from "express";
-import { User } from "../interfaces/users";
+// import { User } from "../interfaces/users";
+
+export const get_root = (_req: Request, res: Response) => {
+    res.send("Hola desde controller");
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { get_root } from "../controllers/users";
 const router = Router();
 
-router.get("/");
+router.get("/", get_root);
 
 export default router;
